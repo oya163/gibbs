229c04379a3fd0a8dff5a6c43505a10731ad5187
Oyashi <oyeshin@hotmail.com>
2020-09-11 01:08:59 -0400

    Fixed yml file and fixed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="gibbs",
     version="0.0.1",
     author="Oyesh Mann Singh",
     author_email="osingh1@umbc.edu",
     description="Non-parametric bayesian segmentation model",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/oya163/gibbs",
     packages=setuptools.find_packages(),
     classifiers=[
